90621db5bc755058da344906166f99bfbe7adfa4
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2023-07-21 11:14:19 +0200

    wip

--- src/components/Features/template.jsx
+++ src/components/Features/template.jsx
@@ -25,6 +25,11 @@ export const FeaturesBlockTemplate = {
           label: "Description",
           type: "rich-text",
         },
+        {
+          name: "image",
+          label: "Image",
+          type: "image",
+        },
       ],
     },
   ],
